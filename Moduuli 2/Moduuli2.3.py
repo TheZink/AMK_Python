984b948b042f4feade7b564e4823ea8b52889392
TheZink <ilkka.sinkonen@gmail.com>
2024-08-21 19:28:10 +0300

    Viikkotehtävä 1

---
+++ Moduuli 2/Moduuli2.3.py
@@ -0,0 +1,7 @@
+luku1 = int(input("Anna suorakulmion kanta: "))
+luku2 = int(input("Anna suorakulmion korkeus: "))
+
+pinta = luku1 * luku2
+piiri = 2 * (luku1 + luku2)
+
+print(f"Suorakulmion pinta-ala on {pinta} ja piiri on {piiri}")
