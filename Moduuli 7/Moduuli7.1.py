a7121c8cf7d8c73af61e2e69a424408913424c52
TheZink <ilkka.sinkonen@gmail.com>
2024-09-10 11:43:34 +0300

    Viikkotehtävä 4

---
+++ Moduuli 7/Moduuli7.1.py
@@ -0,0 +1,7 @@
+kklista = {"talvi":{12:"Joulukuu",1:"Tammikuu",2:"Helmikuu"},"Kevät":{3:"Maaliskuu", 4:"Huhtikuu",5:"Toukokuu"},"Kesä":{6:"Kesäkuu",7:"Heinäkuu",8:"Elokuu"},"Syksy":{9:"Syyskuu",10:"Elokuu",11:"Marraskuu"}}
+syote = int(input("Anna luku: "))
+
+for vuodenajat, kuukaudet in kklista.items():
+    for x in kuukaudet:
+        if x == syote:
+            print(kuukaudet[x])
